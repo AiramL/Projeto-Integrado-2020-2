fd6738be9c0824899460acb3cd0fceb9d075e9cb
Vinícius Figueiredo <megavinoba@gmail.com>
2021-05-02 18:10:43 -0300

    Add entire symptons list

--- src/api/application.py
+++ src/api/application.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
-from flask_wtf import FlaskForm
-from wtforms import widgets, RadioField, SelectMultipleField, SubmitField, SelectField
+from flask_wtf import FlaskForm, Form
+from wtforms.validators import DataRequired
+from wtforms import widgets, SelectMultipleField, SubmitField, SelectField
+import os
+from pickle import load
+import pathlib
 
 SECRET_KEY = 'development'
+symptons_path = '/Users/vinob/Projeto-Integrado-2020-2/data/datasets/transformed/dataset_dummy'
 
 bootstrap = Bootstrap()
 application = Flask(__name__)
@@ -22,10 +27,19 @@ class SimpleForm(FlaskForm):
     # example = RadioField('Label')
 
 
+def predict_disease(symptons):
+    if "febre" in symptons:
+        return "dengue"
+    else:
+        return "..."
+
+
 @application.route('/', methods=['post', 'get'])
 def hello_world():
     form = SimpleForm()
-    symptons = ["febre", "dor_de_cabeca", "coriza", "tosse"]
+    file = os.path.join(pathlib.Path(__file__), symptons_path)
+    symptons = sorted([x.strip() for x in list(load(open(file, 'rb')).keys()) if
+                       type(x) == str])
     form.symptons.choices = [(s, s) for s in symptons]
 
     return render_template('home.html', form=form)
@@ -33,5 +47,4 @@ def hello_world():
 
 @application.route('/predict', methods=['post'])
 def predict():
-    return str(request.form.getlist('symptons'))
-
+    return predict_disease(request.form.getlist('symptons'))
