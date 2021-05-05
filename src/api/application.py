e8c55d6dbf256a2973a89edda4dc10f8ff55837f
Vinícius Figueiredo <megavinoba@gmail.com>
2021-05-05 08:00:16 -0300

    Changes application to include model prediction

--- src/api/application.py
+++ src/api/application.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template, request
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm, Form
-from wtforms.validators import DataRequired
+import sklearn
 from wtforms import widgets, SelectMultipleField, SubmitField, SelectField
 import os
 from pickle import load
 import pathlib
 
 SECRET_KEY = 'development'
-symptons_path = '/Users/vinob/Projeto-Integrado-2020-2/data/datasets/transformed/dataset_dummy'
+model_path = '/Users/vinob/Projeto-Integrado-2020-2/model/model'
+model_file = os.path.join(pathlib.Path(__file__), model_path)
+model = load(open(model_file, 'rb'))
+
+symptons_path = '/Users/vinob/Projeto-Integrado-2020-2/data/datasets/transformed/dummy_dataset_pt_translation'
+diseases_pt_br_path = '/Users/vinob/Projeto-Integrado-2020-2/data/datasets/transformed/dummy_class_pt_translation'
+diseases_en_us_path = '/Users/vinob/Projeto-Integrado-2020-2/data/datasets/transformed/class_dummy'
 
 symptons_file = os.path.join(pathlib.Path(__file__), symptons_path)
-symptons = sorted([x.strip() for x in list(load(open(symptons_file, 'rb')).keys()) if
-                   type(x) == str])
+symptons = list(load(open(symptons_file, 'rb')).items())
+translate_symptons_dict = {int(i): s for i, s in symptons if int(i) != 0}
+
+diseases_file = os.path.join(pathlib.Path(__file__), diseases_en_us_path)
+diseases_en_us = list(load(open(diseases_file, 'rb')).keys())
+diseases_file = os.path.join(pathlib.Path(__file__), diseases_pt_br_path)
+diseases_pt_br = list(load(open(diseases_file, 'rb')).values())
+translate_disease_dict = {k: v for k, v in zip(diseases_en_us, diseases_pt_br)}
 
 bootstrap = Bootstrap()
 application = Flask(__name__)
@@ -32,23 +44,28 @@ class SimpleForm(FlaskForm):
 
 
 def predict_disease(symptons):
-    if "febre" in symptons:
-        return "dengue"
-    else:
-        return "..."
+    input_list = symptons + [0] * (17 - len(symptons))
+    print(input_list)
+    prediction = model.predict([input_list])[0]
+    return prediction
+
+
+def translate_disease(disease_in_english):
+    return translate_disease_dict[disease_in_english]
 
 
 @application.route('/', methods=['post', 'get'])
 def hello_world():
     form = SimpleForm()
-    file = os.path.join(pathlib.Path(__file__), symptons_path)
-    symptons = sorted([x.strip() for x in list(load(open(file, 'rb')).keys()) if
-                       type(x) == str])
-    form.symptons.choices = [(s, s) for s in symptons]
+    form.symptons.choices = [(int(i), s) for i, s in symptons if int(i) != 0]
 
     return render_template('home.html', form=form)
 
 
 @application.route('/predict', methods=['post'])
 def predict():
-    return predict_disease(request.form.getlist('symptons'))
+    sintomas = [translate_symptons_dict[int(x)] for x in request.form.getlist('symptons')]
+    doenca = translate_disease(predict_disease(request.form.getlist('symptons')))
+    frase = 'Seus sintomas são ' + ', '.join(sintomas) + '.\n\nNosso sistema acredita que você esteja com ' + doenca + '.'
+
+    return frase
